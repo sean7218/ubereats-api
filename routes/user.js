cf76fe278b4de9ad86d1f3b1332163fd21fb5cb3
sean7218 <sean7218@gmail.com>
2018-08-11 13:41:47 -0400

    user connect to db

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+var bodyParser = require('body-parser');
 
-/* GET users listing. */
+router.use(bodyParser.json());
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  models.User.findAll().then(function(users){
+    res.json(users);
+  });
+});
+
+router.get('/:id', function(req, res, next) {
+  let email = req.params.email;
+  console.log(email);
+  models.User.findById(req.params.id).then(function(user){
+    return res.send(user);
+  });
+});
+
+router.put('/:id', function(req, res){
+    models.User.findById(req.params.id)
+    .then(function(user){
+        user.update({
+            name: req.body.name,
+            phone: req.body.phone,
+        });
+        return res.send(`user ${req.body.name} updated`);
+    });
+});
+
+router.delete('/:id', function(req, res){
+    models.User.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(function(user){
+        return res.send(`the user ${req.params.id} has been deleted`);
+    });
 });
 
 module.exports = router;
