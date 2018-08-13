f30d5f744f0b57740c57d2ea54c7bf7d11a676ee
sean7218 <sean7218@gmail.com>
2018-08-13 08:38:14 -0400

    done the test eslint

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -1,27 +1,27 @@
-const userRouter = require('../routes/user');
-const express = require('express');
-const request = require('supertest');
+const userRouter = require('../routes/user')
+const express = require('express')
+const request = require('supertest')
 
-jest.mock('../models');
+jest.mock('../models')
 
 const initApp = () => {
-  const app = express();
-  app.use('/user', userRouter);
-  return app;
+  const app = express()
+  app.use('/user', userRouter)
+  return app
 }
 
 describe('GET /user', () => {
   test('It should fetch all users', async () => {
-    const app = initApp();
-    const res = await request(app).get('/user');
+    const app = initApp()
+    const res = await request(app).get('/user')
     expect(res.body).toEqual({
-      "createdAt": "2018-08-11",
-      "email": "da@gmail.com",
-      "id": 8,
-      "name": "da",
-      "password": "$2a$10$5Dx/1.SrUNsOpTCvTyvLCuxqqxVx.piqD5pMGVIPiNPp9UdDiB38O",
-      "phone": 1231231122,
-      "updatedAt": "2018-08-11"
-    });
-  });
-});
+      'createdAt': '2018-08-11',
+      'email': 'da@gmail.com',
+      'id': 8,
+      'name': 'da',
+      'password': '$2a$10$5Dx/1.SrUNsOpTCvTyvLCuxqqxVx.piqD5pMGVIPiNPp9UdDiB38O',
+      'phone': 1231231122,
+      'updatedAt': '2018-08-11'
+    })
+  })
+})
--- __tests__/user_dev.test.js
+++ __tests__/user_dev.test.js
@@ -1,17 +1,21 @@
-var request = require('request');
+var request = require('request')
 
 const fetchData = () => {
-  return new Promise(function(resolve, reject){
+  return new Promise(function (resolve, reject) {
     request('http://localhost:3000/user', (err, res, body) => {
-      resolve(res);
-    });
-  });
+      if (err) {
+        console.log(err)
+        reject(err)
+      }
+      resolve({res: res, body: body})
+    })
+  })
 }
 
 describe('test the api endpoint', () => {
   test('it should get data for the user', () => {
-    fetchData().then(res => {
-      expect(res.statusCode).toBe(200);
+    fetchData().then(obj => {
+      expect(obj.res.statusCode).toBe(200)
     })
-  });
-});
+  })
+})
